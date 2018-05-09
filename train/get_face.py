6a5a3329e53269152d8890dddcb48db18d1815fc
El-Chiang <jing1downey@gmail.com>
2018-05-09 14:08:41 +0800

    添加转灰度图函数

--- train/get_face.py
+++ train/get_face.py
@@ -1,4 +1,8 @@
 # coding=utf-8
+import numpy as np
+import skimage.io
+from PIL import Image
+
 import cv2
 
 # Loading cat-face detector
@@ -32,7 +36,21 @@ def face_rect(faces, img):
         cv2.putText(img, 'Cat', (x, y - 7), 3, 1.2,
                     (0, 255, 0), 2, cv2.LINE_AA)
     cv2.imshow('Cat?', img)
-    c = cv2.waitKey(0)
+    cv2.waitKey(0)
+
+
+def get_bin_graph(img_name):
+    ''' convert to bin graph '''
+    img = Image.open(img_name)
+    img = img.convert('L')  # convert to grey-scale graph
+    imgs = skimage.io.imread(img_name)
+    ttt = np.mean(imgs)
+
+    WHITE, BLACK = 255, 0
+    img = img.point(lambda x: WHITE if x > ttt else BLACK)
+    img = img.convert('1')
+    img.save('new_' + img_name)
+
 
 def cut_img():
     '''Cut cat face'''
@@ -55,6 +73,7 @@ def cut_img():
             cut_img = img[y: y + h, x: x + w]
         cv2.imwrite('traindata/yebi/face' + str(img_name) + '.jpg', cut_img)
         img_name += 1
+
 
 if __name__ == '__main__':
     cut_img()
